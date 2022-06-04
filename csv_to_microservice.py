a9d41095466fa51f5693a54d9dca28294f25bd0e
Mohammad Javad Tatari <mjavadtatari98@gmail.com>
2022-06-04 20:17:36 +0430

    some changes

--- csv_to_microservice.py
+++ csv_to_microservice.py
@@ -1,11 +1,12 @@
 import csv
 from microservice import Microservice
+from microservice_group import MicroserviceGroup
 
 # global variables
 CSV_FILE_ADDRESS = 'sample-data.csv'
 
 rows_without_headers = []
-microservices_list = []
+microservicesgroup_list = []
 
 with open(CSV_FILE_ADDRESS, 'r') as f:
     for i in csv.reader(f):
@@ -13,9 +14,18 @@ with open(CSV_FILE_ADDRESS, 'r') as f:
 
     rows_without_headers.pop(0)
 
+for col in range(1,13):
+    microservices_list = []
+    for i in rows_without_headers:
+        microservices_list.append(Microservice(i[col]))
+    microservicesgroup_list.append(MicroserviceGroup(microservices_list))
+    
 
-for i in rows_without_headers:
-    microservices_list.append(Microservice(i[1:]))
 
-for i in microservices_list:
-    print(i.to_str())
+# for i in range(len(microservicesgroup_list)):
+#     print('{0}: {1}'.format(i, microservicesgroup_list[i]))
+
+
+def get_data():
+    global microservicesgroup_list
+    return microservicesgroup_list
